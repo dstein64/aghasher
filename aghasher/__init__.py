314394a7cfa8dd734053bae992a01d83d594927b
Dan <dstein64@users.noreply.github.com>
2019-11-07 00:58:12 -0500

    Add files for packaging.

---
+++ aghasher/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+from .core import AnchorGraphHasher
+
+version_txt = os.path.join(os.path.dirname(__file__), 'version.txt')
+with open(version_txt, 'r') as f:
+    __version__ = f.read().strip()
