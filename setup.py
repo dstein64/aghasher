aadd0dbfcd271120ddc02354d51ba111653d87f0
Dan <dstein64@users.noreply.github.com>
2020-08-26 20:30:22 -0400

    Use a context manager for reading README.md.

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ version_txt = os.path.join(os.path.dirname(__file__), 'aghasher', 'version.txt')
 with open(version_txt, 'r') as f:
     version = f.read().strip()
 
+with io.open('README.md', encoding='utf8') as f:
+    long_description = f.read()
+
 setup(
     author='Daniel Steinberg',
     author_email='ds@dannyadam.com',
@@ -28,7 +31,7 @@ setup(
     install_requires=['numpy', 'scipy'],
     keywords=['anchor-graph-hashing', 'hashing', 'locality-sensitive-hashing', 'machine-learning'],
     license='MIT',
-    long_description=io.open('README.md', encoding='utf8').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     name='aghasher',
     package_data={'aghasher': ['version.txt']},
